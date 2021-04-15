222bbf5878252c95f501cc62f06bf5c6f08bade4
0elle0 <el_leo_m@hotmail.com>
2021-04-15 20:27:00 +0200

    fix input bug

--- static/js/canvas.js
+++ static/js/canvas.js
@@ -1,31 +1,30 @@
-
 const getUserInput = () =>  {
 
     let x1 = 0, y1 = 0, x2 = 0, y2 = 0, x3 = 0, y3 = 0;
-    let coordinates = [x1, y1, x2, y2, x3, y3]
-    let userInput = []
+    let userInput = [x1, y1, x2, y2, x3, y3]
     let inputs = document.getElementsByTagName('input')
 
     let i = 0
 
-    coordinates.forEach(element => {
+    userInput.forEach(element => {
         element = inputs[i].value
-        console.log(inputs[i].value)
+        //console.log(inputs[i].value)
+        userInput[i] = element;
         i++
-        userInput.push(element)
-    });
-    console.log('click')
 
-    drawTriangle()
+    });
+
+    console.log(userInput.length);
+
+
+    drawTriangle(userInput)
 
 
 }
-const drawTriangle = () => {
+const drawTriangle = (userInput) => {
     let canvas = document.querySelector("#myCanvas");
     let context = canvas.getContext('2d');
-
-    let x1 = 50;
-    let y1 = 0;
+    console.log(userInput)
 
     context.clearRect(0, 0, canvas.width, canvas.height);
 
@@ -36,9 +35,9 @@ const drawTriangle = () => {
     let height = (x1-y1) * Math.cos(Math.PI / 6);
 
     context.beginPath();
-    context.moveTo(x1, 500);
-    context.lineTo(700, 300);
-    context.lineTo(200, 300 - height);
+    context.moveTo(userInput[0], userInput[1]);
+    context.lineTo(userInput[2], userInput[3]);
+    context.lineTo(userInput[4], userInput[5]);
     context.closePath();
 
     // border
@@ -56,7 +55,7 @@ const drawAxis = () => {
 
     let canvas = document.querySelector("#canvasX");
     let contextAxisX = canvas.getContext('2d');
-    contextAxisX.moveTo(25, 50);
+    contextAxisX.moveTo(0, 50);
     contextAxisX.lineTo(1000, 50);
     contextAxisX.stroke();
     contextAxisX.font = '24px Arial'
@@ -69,14 +68,14 @@ const drawAxis = () => {
         contextAxisX.moveTo(x, 42);
         contextAxisX.lineTo(x, 50);
         contextAxisX.stroke();
-        contextAxisX.fillText(x.toString(), x, 40);
+        contextAxisX.fillText(x.toString(), x+50, 40);
     }
 
     canvas = document.querySelector("#canvasY");
     let contextAxisY = canvas.getContext('2d');
 
-    contextAxisY.moveTo(25, 0);
-    contextAxisY.lineTo(25, 570);
+    contextAxisY.moveTo(50, 0);
+    contextAxisY.lineTo(50, 570);
     contextAxisY.stroke();
     contextAxisY.font = '14px Arial'
     contextAxisY.fillText("y", 1, 590);
@@ -84,10 +83,10 @@ const drawAxis = () => {
     contextAxisX.font = '14px Arial'
     for (let y = 50; y <= 550; y += 50) {
 
-        contextAxisY.moveTo(20, y);
-        contextAxisY.lineTo(30, y);
+        contextAxisY.moveTo(40, y);
+        contextAxisY.lineTo(50, y);
         contextAxisY.stroke();
-        contextAxisY.fillText(y.toString(), 0, y - 5);
+        contextAxisY.fillText(y.toString(), 25, y - 5);
     }
 }
 
